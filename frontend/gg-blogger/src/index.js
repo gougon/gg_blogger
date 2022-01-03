0358c8838d264cdd8cb0eb3314846d59a3143803
gougon <freddy2531@gmail.com>
2022-01-03 19:20:18 +0800

    feature/basic setting frontend & backend

--- frontend/gg-blogger/src/index.js
+++ frontend/gg-blogger/src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './js/App';
+import reportWebVitals from './js/reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App/>,
   document.getElementById('root')
 );
 
